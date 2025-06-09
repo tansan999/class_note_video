209ded7b1e509108a1a3f76e832505f6cc74bc6c
Kudaibergen <kudash1525@gmail.com>
2025-06-09 16:21:34 +0600

    измениние  сылки

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import GlobalStyles, { lightTheme, darkTheme } from './styles/GlobalStyles';
-import { AuthProvider } from './context/AuthContext';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { ThemeProvider } from './context/ThemeContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import GlobalStyles, { lightTheme, darkTheme } from "./Styles/GlobalStyles";
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { ThemeProvider } from "./context/ThemeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider>
     <AuthProvider>
